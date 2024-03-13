b5ad77f881fedee86de582b58eae4d62cdc7d366
Keerthi Avirneni <123keerthi45@gmail.com>
2024-03-13 15:54:43 +0000

    index js

--- ai_nft_generator-master/src/components/Navigation.js
+++ ai_nft_generator-master/src/components/Navigation.js
@@ -10,7 +10,7 @@ const Navigation = ({ account, setAccount }) => {
     return (
         <nav>
             <div className='nav__brand'>
-                <h1>AI NFT Generator</h1>
+                <h1>AI NFT MINTING MARKETPLACE</h1>
             </div>
 
             {account ? (
